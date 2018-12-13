ae7966aa6791aa5f8f98be8e99aeff68638414b8
Cheree Nielson <38470257+ChereeNielson@users.noreply.github.com>
2018-12-12 17:58:23 -0700

    html push

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,56 +1,59 @@
-// Initialize Firebase
+// Initialize Firebase //
 var config = {
-    apiKey: "AIzaSyBnKaz97Ae8XTn03c5KV-uKbAPFluF6NAc",
-    authDomain: "intro-to-firebase-98eac.firebaseapp.com",
-    databaseURL: "https://intro-to-firebase-98eac.firebaseio.com",
-    projectId: "intro-to-firebase-98eac",
-    storageBucket: "intro-to-firebase-98eac.appspot.com",
-    messagingSenderId: "164243903301"
-};
+    apiKey: "AIzaSyAjzWICVCt8OM8QB-U-ZFFPO930amTcoz4",
+    authDomain: "train-scheduler-5fdce.firebaseapp.com",
+    databaseURL: "https://train-scheduler-5fdce.firebaseio.com",
+    projectId: "train-scheduler-5fdce",
+    storageBucket: "train-scheduler-5fdce.appspot.com",
+    messagingSenderId: "512269028262"
+  };
+  
 firebase.initializeApp(config);
+
 var database = firebase.database();
 var name = "";
-var role = "";
-var startDate = "";
-var monthsWorked = "";
-var monthlyRate = "";
-var totalBilled = "";
-// var randomDate = "02/23/1999";
-// var randomFormat = "DD/MM/YYYY";
+var destination = "";
+var time = "";
+var frequency = "";
+var nextArrival = "";
+var minutesAway = "";
 // var convertedDate = moment(randomDate, rendomFormat);
-$("#add-user").on("click", function(event) {
+
+$("#add-train").on("click", function(event) {
     event.preventDefault();
-    console.log(name)
-    console.log(role)
-    console.log(startDate);
-    console.log(monthsWorked);
-    console.log(monthlyRate);
-    console.log(totalBilled);
+    console.log(name);
+    console.log(destination);
+    console.log(time);
+    console.log(frequency);
+    console.log(nextArrival);
+    console.log(minutesAway);
     name = $("#name-input").val().trim();
-    role = $("#role-input").val().trim();
-    startDate = $("#startDate-input").val().trim();
-    monthsWorked = 0;
-    monthlyRate = $("#monthlyRate-input").val().trim();
-    totalBilled = 0;
-    $("#tableBody").append("<tr> <td>" + name + "</td> <td>" + role + "</td> <td>" + startDate + "</td> <td>" + monthsWorked + "</td> <td>" + monthlyRate + "</td> <td>" + totalBilled + "</td> </tr>");
+    destination = $("#destination-input").val().trim();
+    time = $("#time-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
+    nextArrival = 0;
+    minutesAway = 0;
+    $("#tableBody").append("<tr> <td>" + name + "</td> <td>" + destination + "</td> <td>" + time + "</td> <td>" + frequency + "</td> <td>" + nextArrival + "</td> <td>" + minutesAway + "</td> </tr>");
     database.ref().push({
         name: name,
-        role: role,
-        startDate: startDate,
-        monthsWorked: monthsWorked,
-        monthlyRate: monthlyRate,
-        totalBilled: totalBilled,
+        destination: destination,
+        time: time,
+        frequency: frequency,
+        nextArrival: nextArrival,
+        minutesAway: minutesAway,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
-    $("#name-input, #role-input, #startDate-input, #monthsWorked-input, #monthlyRate-input, #totalBilled-input").val('');
+    $("#name-input, #destination-input, #time-input, #frequency-input").val('');
 });
-database.ref().on("child_added", function (childSnapshot) {
+
+database.ref().on("child_added", function(childSnapshot) {
     console.log(childSnapshot.val().name);
-    console.log(childSnapshot.val().role);
-    console.log(childSnapshot.val().startDate);
-    console.log(childSnapshot.val().monthsWorked);
-    console.log(childSnapshot.val().monthlyRate);
-    console.log(childSnapshot.val().totalBilled);
+    console.log(childSnapshot.val().destination);
+    console.log(childSnapshot.val().time);
+    console.log(childSnapshot.val().frequency);
+    console.log(childSnapshot.val().nextArrival);
+    console.log(childSnapshot.val().minutesAway);
+
     // // Change the HTML to reflect
     // $("#name-input").text(childSnapshot.name);
     // $("#role-input").text(childSnapshot.role);
@@ -59,18 +62,19 @@ database.ref().on("child_added", function (childSnapshot) {
     // $("#monthlyRate-input").text(childSnapshot.monthlyRate);
     // $("#totalBilled-input").text(childSnapshot.totalBilled);
     $("#tableBody").append(
-        "<tr class='well'><td class='member-name'> " + childSnapshot.val().name +
-        " </td><td class='member-role'> " + childSnapshot.val().role +
-        " </td><td class='member-startDate'> " + childSnapshot.val().startDate +
-        " </td><td class='member-monthsWorked'> " + childSnapshot.val().monthsWorked +
-        " </td><td class='member-monthlyRate'> " + childSnapshot.val().monthlyRate +
-        " </td><td class='member-totalBilled'> " + childSnapshot.val().totalBilled +
+        "<tr class='well'><td class='train-name'> " + childSnapshot.val().name +
+        " </td><td class='train-destination'> " + childSnapshot.val().destination +
+        " </td><td class='train-time'> " + childSnapshot.val().time +
+        " </td><td class='train-frequency'> " + childSnapshot.val().frequency +
+        " </td><td class='train-nextArrival'> " + childSnapshot.val().nextArrival +
+        " </td><td class='train-minutesAway'> " + childSnapshot.val().minutesAway +
         " </td></tr>");
     // Handle the errors
-}, function (errorObject) {
+}, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
 });
-database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function (snapshot) {
+
+database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
     //   // Change the HTML to reflect
     // $("#name-input").text(snapshot.val().name);
     // $("#role-input").text(snapshot.val().role);
